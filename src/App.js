b54bfa27828ace75ae839088db7beb8f7775a958
WafAws <wafalab@amazon.com>
2023-12-14 11:22:12 +0000

    Your Message

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import React, { useEffect, useState } from 'react';
-
-function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('/api')
-      .then((response) => response.text())
-      .then((data) => setMessage(data));
-  }, []);
-
-  return (
-    <div>
-      <h1>Create React App with Express</h1>
-      <p>{message}</p>
-    </div>
-  );
-}
-
-export default App;
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
